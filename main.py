b411f1998417b554c65c00218004b4466be317b0
Kitsu <mail@kitsu.me>
2023-02-06 01:36:18 +0300

    Use argostranslate instead of googletrans

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from PIL import Image
 import pyocr
 import pyocr.builders
 import pyscreenshot
-from googletrans import Translator
+import argostranslate.package
+import argostranslate.translate
 import PySimpleGUI as sg
 
 parser = argparse.ArgumentParser(description='Translate selected text.')
@@ -29,10 +30,22 @@ txt = tool.image_to_string(
 # newlines are not handled properly so just join all the lines
 txt = ' '.join([line.strip() for line in txt.split('\n')])
 
-if args.src is not None:
-    text = Translator().translate(txt, dest=args.dest, src=args.src).text
-else:
-    text = Translator().translate(txt, dest=args.dest).text
+argostranslate.package.update_package_index()
+available_packages = argostranslate.package.get_available_packages()
+package_to_install = next(
+    filter(
+        lambda x: x.from_code == args.src and x.to_code == args.dest, available_packages
+    )
+)
+if package_to_install is None:
+    print("Not found language package for {} -> {}".format(args.src, args.dest))
+    exit(1)
+
+if package_to_install not in argostranslate.package.get_installed_packages():
+    argostranslate.package.install_from_path(package_to_install.download())
+
+# Translate
+text = argostranslate.translate.translate(txt, args.src, args.dest)
 
 sg.popup(text,
          title='onscreen-translate-py',
